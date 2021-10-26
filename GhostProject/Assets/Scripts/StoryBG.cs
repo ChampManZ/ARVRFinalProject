f24da29c671d8435bb3bb3c85d6e48fa1dc41cbb
punchnnp <punch.nunnapat@gmail.com>
2021-10-26 17:15:27 +0700

    story bg

--- GhostProject/Assets/Scripts/StoryBG.cs
+++ GhostProject/Assets/Scripts/StoryBG.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class StoryBG : MonoBehaviour
 {
-    public Sprite BG;
-    public Sprite firstBG;
-    public Sprite secondBG;
-    public Sprite thirdBG;
-    public Sprite fourthBG;
-    public Sprite fifthBG;
-    public Sprite sixthBG;
-    public SpriteRenderer spriteRenderer;
+    public GameObject BG;
+    public GameObject firstBG;
+    public GameObject secondBG;
+    public GameObject thirdBG;
+    public GameObject fourthBG;
+    public GameObject fifthBG;
+    public GameObject sixthBG;
+    //public SpriteRenderer spriteRenderer;
 
     [SerializeField] float timer = 0;
 
     void Start()
     {
-        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        //spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
     // Update is called once per frame
     void Update()
@@ -26,36 +26,86 @@ public class StoryBG : MonoBehaviour
         
         if (timer >= 4)
         {
-           this.spriteRenderer.sprite = firstBG;
+           //this.spriteRenderer.sprite = firstBG;
+           firstBG.SetActive(true);
+           secondBG.SetActive(false);
+           thirdBG.SetActive(false);
+           fourthBG.SetActive(false);
+           fifthBG.SetActive(false);
+           sixthBG.SetActive(false);
+           BG.SetActive(false);
         }
 
         if (timer >= 9)
         {
-            this.spriteRenderer.sprite = secondBG;
+            //this.spriteRenderer.sprite = secondBG;
+            firstBG.SetActive(false);
+           secondBG.SetActive(true);
+           thirdBG.SetActive(false);
+           fourthBG.SetActive(false);
+           fifthBG.SetActive(false);
+           sixthBG.SetActive(false);
+           BG.SetActive(false);
         }
 
         if (timer >= 19)
         {
-            this.spriteRenderer.sprite = thirdBG;
+            //this.spriteRenderer.sprite = thirdBG;
+            firstBG.SetActive(false);
+           secondBG.SetActive(false);
+           thirdBG.SetActive(true);
+           fourthBG.SetActive(false);
+           fifthBG.SetActive(false);
+           sixthBG.SetActive(false);
+           BG.SetActive(false);
         }
 
         if (timer >= 24)
         {
-            this.spriteRenderer.sprite = fourthBG;
+            //this.spriteRenderer.sprite = fourthBG;
+            firstBG.SetActive(false);
+           secondBG.SetActive(false);
+           thirdBG.SetActive(false);
+           fourthBG.SetActive(true);
+           fifthBG.SetActive(false);
+           sixthBG.SetActive(false);
+           BG.SetActive(false);
+
 
             if (timer >= 26) {
-                this.spriteRenderer.sprite = fifthBG;
+                //this.spriteRenderer.sprite = fifthBG;
+                firstBG.SetActive(false);
+           secondBG.SetActive(false);
+           thirdBG.SetActive(false);
+           fourthBG.SetActive(false);
+           fifthBG.SetActive(true);
+           sixthBG.SetActive(false);
+           BG.SetActive(false);
+
             }
         }
 
         if (timer >= 28)
         {
-            this.spriteRenderer.sprite = BG;
+            //this.spriteRenderer.sprite = BG;
+            firstBG.SetActive(false);
+           secondBG.SetActive(false);
+           thirdBG.SetActive(false);
+           fourthBG.SetActive(false);
+           fifthBG.SetActive(false);
+           sixthBG.SetActive(false);
+           BG.SetActive(true);
         }
 
         if (timer >= 37)
         {
-            this.spriteRenderer.sprite = sixthBG;
+            //this.spriteRenderer.sprite = sixthBG;firstBG.SetActive(false);
+           secondBG.SetActive(false);
+           thirdBG.SetActive(false);
+           fourthBG.SetActive(false);
+           fifthBG.SetActive(false);
+           sixthBG.SetActive(true);
+           BG.SetActive(false);
         }
     }
 }
